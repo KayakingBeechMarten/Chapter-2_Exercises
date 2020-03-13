4fabfd180bb992787e553d40013c6aeac6c7b378
KayakingBeechMarten <60995281+KayakingBeechMarten@users.noreply.github.com>
2020-03-13 01:28:39 -0700

    Add files via upload

---
+++ Ch2 Exce/Ch2 Exc1/Ch2 Exc1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*Find the average of 3 fixed numbers
+
+The fixed numbers are 10, 100, and 100
+
+Perform math
+Avg = (10+100+1000) / 3
+
+Print Average*/
+
+int main() {
+	int Average = (10 + 100 + 1000) / 3;
+
+	std::cout << Average;
+}
---
+++ Ch2 Exce/Ch2 Exc2/Ch2 Exc2.cpp
@@ -0,0 +1,13 @@
+/*Sum and Average of 3 Exam Scores
+
+Require user input the score of Test 1,
+Then input of Test 2's score,
+and then input of Test 3's score
+
+Perform math functions:
+Sum = Test1 + Test2 + Test3
+
+Average = Sum / 3
+
+Print Sum, next line
+Print Average*/
